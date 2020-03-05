9fd8c002cc4d3c14ffd11a21ce84aaf31d6f55ee
mamtys <Mamtysman@gmail.com>
2020-03-05 17:20:05 +0300

    feat: add count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,6 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  return (matrix || [])
+    .reduce((flatten, el) => flatten.concat(el), [])
+    .filter(el => el === '^^')
+    .length;
 };
