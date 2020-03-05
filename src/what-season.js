430f4e585ef0790537e481f154e3946f09bccea6
mamtys <Mamtysman@gmail.com>
2020-03-05 19:19:29 +0300

    feat: add what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,25 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date === undefined) {
+    return 'Unable to determine the time of year!';
+  }
+  if (!date instanceof Date) {
+    throw new Error('wrong argument');
+  }
+  if (Object.prototype.hasOwnProperty.call(date, 'getMonth')) {
+    throw new Error('wrong argument');
+  }
+
+  const month = date.getMonth();
+  if (month >= 11 || month <=1) {
+    return 'winter';
+  }
+  if (month >= 8) {
+    return 'autumn'
+  }
+  if (month >= 5) {
+    return 'summer';
+  }
+  if (month >=2) {
+    return 'spring';
+  }
 };
