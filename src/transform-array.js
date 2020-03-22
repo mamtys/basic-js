c0c77e9413d48ad3cc601014b69072bd526c2985
mamtys <Mamtysman@gmail.com>
2020-03-22 23:57:48 +0300

    feat: add transform-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,19 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)){
+        throw new Error();
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] in TRANSFORMATION_TABLE) {
+            TRANSFORMATION_TABLE[arr[i]](arr, i)
+        }
+    }
+    return arr;
 };
+
+const TRANSFORMATION_TABLE = {
+    '--discard-next': (arr, ind)  => arr.splice(ind, 2),
+    '--discard-prev': (arr, ind)  => arr.splice(ind - 1 > 0 ? ind - 1 : ind , 2),
+    '--double-next': (arr, ind)  => arr.splice(ind, 1, arr[ind+1]),
+    '--double-prev': (arr, ind)  =>arr.splice(ind, 1, arr[ind-1]),
+}
