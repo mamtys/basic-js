d63c95625c4a58501aa92d15158f3399b81aa4bb
mamtys <Mamtysman@gmail.com>
2020-03-22 15:34:27 +0300

    feat: add extended-repeator solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,12 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(
+    str, {
+        repeatTimes = 1,
+        separator = '+',
+        addition = '',
+        additionRepeatTimes = 1,
+        additionSeparator = '|'
+    }) {
+
+    const add = (addition + additionSeparator).repeat(additionRepeatTimes).slice(0, -additionSeparator.length);
+    return (str + add + separator).repeat(repeatTimes).slice(0, -separator.length);
 };
-  
