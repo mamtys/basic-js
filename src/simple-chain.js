74c3c79173fe9b07f4aa357d5cc4f51eb157058a
mamtys <Mamtysman@gmail.com>
2020-03-22 19:17:11 +0300

    feat: add simple-chain solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,63 @@
+const storage = Symbol('storage');
 const chainMaker = {
+  [storage]: [],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this[storage].length;
   },
+
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this[storage].push(String(value));
+
+    return this;
   },
+
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this._validatePosition(position);
+    this[storage].splice(position - 1, 1);
+
+    return this;
   },
+
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this[storage].reverse();
+
+    return this;
   },
+
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    const result = this[storage]
+      .map(el => `~( ${el} )~`)
+      .join('')
+      .slice(1, -1);
+
+    this[storage] = [];
+
+    return result;
+
+  },
+
+  _validatePosition(position) {
+    if (!Number.isInteger(position)) {
+      this._clearStorage();
+      throw new Error('validation error');
+    }
+
+    if (position < 1) {
+      this._clearStorage();
+      throw new Error('validation error');
+    }
+
+    if (position >= this[storage].length) {
+      this._clearStorage();
+      throw new Error('validation error')
+    }
+
+    return true;
+  },
+
+  _clearStorage() {
+    this[storage] = [];
   }
 };
 
